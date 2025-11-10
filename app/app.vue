97543850751f0dacdbcfb4b13564e13d69b5ee8b
m.shcherba <mykola.shcherba@snagasolutions.com>
2025-11-10 03:57:36 +0200

    init

---
+++ app/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ count }}</p>
+    <button @click="addCount">Add</button>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+const count = ref(0);
+
+function addCount() {
+  count.value += 1;
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  color: red;
+}
+</style>
